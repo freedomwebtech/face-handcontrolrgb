80b01f6e1d0c5a3d992c24f87868b849a7cf99d6
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2022-05-08 08:45:16 +0530

    Update img.py

--- img.py
+++ img.py
@@ -6,7 +6,7 @@ while True:
      cv2.imshow('FRAME',frame)
      if cv2.waitKey(1) == ord('a'):
         print ("pressed a")
-        frame=cv2.imwrite("/home/pi/face-handcontrolrgb/rf.png",frame)
+        frame=cv2.imwrite("/home/pi/handcontrolrgb/rf.png",frame)
         break
         
 cap.release()
